3ff4a748e1872e53e2db16322f6456382b1d7e3c
onizuka <dark_sao@mail.ru>
2021-11-06 14:22:47 +0300

    Add Gird System

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
-#encoding: utf-8
+require 'sinatra/activerecord'
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
+
+#создали две миграции, одна создает таблицу, а вторая наполоняет ее
+#Потом за раз смогли сделать rake db:migrate и создалось две миграции для нащей молдели
+#Если хотим поменять что то в таблице, простло удаляем pizzashop.db и снова rake db:migrate
+class Product < ActiveRecord::Base
+
+end
+
+before do
+	@items = Product.all
+end
 
 get '/' do
 	erb :index
