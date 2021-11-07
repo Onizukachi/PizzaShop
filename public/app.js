31e8cdec47077db462a413bb31a0e7fa50d07a40
onizuka <dark_sao@mail.ru>
2021-11-07 18:33:53 +0300

    Add count a cart

--- public/app.js
+++ public/app.js
@@ -19,11 +19,33 @@ function add_to_cart(id){
 
     var key = 'product_' + id;
 	
-    var x = window.localStorage.getItem(key);
-    
+    var x = window.localStorage.getItem(key);  
     x = x * 1 + 1;
-`   `
-    window.localStorage.setItem(key, x);
 
+    window.localStorage.setItem(key, x);
 }
 
+function cart_get_number_of_items() {
+    //Обьявляем перемунную в которой будем считать общее количество заказов в корзине
+    let count = 0;
+
+    for(let i=0; i<localStorage.length; i++) {
+
+        //Получаем ключ по индексу массива, мы же проходим как в массиве, с 0 элемента
+        let key = localStorage.key(i);
+
+        //Получаем значение(количествой пиццы) по ключу
+        let value = localStorage.getItem(key);
+        
+//Провреряем чтоб ключ начинался на (product_) так как в localstorage  могут другие значения хранится 
+//indexof возвращает индекс вхождения элемента, в нашем случае нужно чтою 0. И превращаем в true
+        //
+        if(key.indexOf('product_') == 0)
+        {
+            //Считаем и с помощщью + приводим value к числу
+            count += (+value);
+        }
+        
+    }
+    return count;
+}
